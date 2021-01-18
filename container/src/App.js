e4083fd53754752e6ae2646c68aa487ef3d7a52e
Ashish Mehra <singhashu163@gmail.com>
2021-01-18 23:16:01 +0530

    Browser router added

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
 export default () => (
-  <>
+  <BrowserRouter>
     <Header />
     <MarketingApp />
-  </>
+  </BrowserRouter>
 );
