56d683dcc042d5e0a323a125bd7129965531a7ea
Ashish Mehra <singhashu163@gmail.com>
2021-01-19 06:25:09 +0530

    Production styling fixed

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import {Switch,Route,BrowserRouter} from 'react-router-dom';
-import {StylesProvider} from "@material-ui/core/styles";
-
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generatedClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 export default () => {
-    return <div>
-        <StylesProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/pricing" component={Pricing} />
-                    <Route path="/" component={Landing} />
-                </Switch>
-            </BrowserRouter>
-        </StylesProvider>
+  return (
+    <div>
+      <StylesProvider generateClassName={generatedClassName}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
-}
+  );
+};
