276563d67a958ef0e932533f652436814b8cbe03
Ashish Mehra <singhashu163@gmail.com>
2021-01-20 14:25:50 +0530

    genrate class name prefix updated in container application

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: "co",
+  productionPrefix: "container",
 });
 
 export default () => (
